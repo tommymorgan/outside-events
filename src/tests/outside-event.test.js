65e7cf98709a1b949a9f2255d3e977b5e1a37afc
Tommy Morgan <tommy@tommymorgan.com>
2020-06-21 11:35:38 -0500

    Improve unit tests, upgrade dependencies, clean up tracked files
    1. Make unit tests a little more outcome driven and less implementation driven.
    2. Describe existing tests as Browser tests in preparation for future tests in support of server side rendering.
    3. Add coverage threshold configuration.
    4. Upgrade dependencies.
    5. Remove coverage report from git tracking.

--- src/tests/outside-event.test.js
+++ src/tests/outside-event.test.js
@@ -5,91 +5,85 @@ import {
 } from "sinon";
 
 let outsideEvents;
-let listener;
 let event;
 let html = `
 	<div class="outer">
 		<div class="main">
-			<div class="nested"></div>
+			<div class="nested">0</div>
 		</div>
 	</div>
 `;
 
-beforeEach(() => {
-	window.MutationObserver = function() {
-		this.observe = stub();
-	};
+describe("Browser", () => {
+	beforeEach(() => {
+		window.MutationObserver = function() {
+			this.observe = stub();
+		};
+		document.body.innerHTML = html;
 
-	document.body.innerHTML = html;
-	listener = stub();
+		let nested = document.querySelector(".nested");
+		let listener = (ev) => {
+			nested.innerHTML = ++nested.innerHTML;
+		};
+	
+		const el = document.querySelector(".main");
+		outsideEvents = new OutsideEvents(el);
+		outsideEvents.addEventListener("click", listener);
 
-	const el = document.querySelector(".main");
-	outsideEvents = new OutsideEvents(el);
-	outsideEvents.addEventListener("click", listener);
-	event = document.createEvent("MouseEvents");
-	event.initEvent("click", true, true);
-});
+		event = document.createEvent("MouseEvents");
+		event.initEvent("click", true, true);
+	});
 
-test("addEventListener adds listener to document", () => {
-	spy(document, "addEventListener");
+	test("addEventListener adds listener meta data to _events array", () => {
+		outsideEvents.addEventListener("mousemove", stub());
+	
+		const found = outsideEvents._events.find((event) => event.type === "mousemove");
+	
+		expect(found).toBeDefined();
+	});
+	
+	test("removeEventListener removes listener from document", () => {
+		spy(document, "removeEventListener");
+	
+		outsideEvents.removeEventListener("click");
+		expect(document.removeEventListener.callCount).toBe(1);
+		expect(document.removeEventListener.args[0][0]).toEqual("click");
+		expect(typeof document.removeEventListener.args[0][1]).toEqual("function");
+	
+		document.removeEventListener.restore();
+	});
+	
+	test("removeEventListener removes listener meta data from _events array", () => {
+		outsideEvents.removeEventListener("mousemove");
+	
+		const found = outsideEvents._events.find((event) => event.type === "mousemove");
+	
+		expect(found).toBeUndefined();
+	});
+	
+	test("nested elements don't trigger listeners", () => {
+		const el = document.querySelector(".nested");
+		el.dispatchEvent(event);
 
-	outsideEvents.addEventListener("mousemove", stub());
-	expect(document.addEventListener.callCount).toBe(1);
-	expect(document.addEventListener.args[0][0]).toEqual("mousemove");
-	expect(typeof document.addEventListener.args[0][1]).toEqual("function");
+		expect(el.innerHTML).toBe("0");
+	});
+	
+	test("events on self don't trigger listeners", () => {
+		const el = document.querySelector(".main");
+		el.dispatchEvent(event);
 
-	document.addEventListener.restore();
-});
-
-test("addEventListener adds listener meta data to _events array", () => {
-	outsideEvents.addEventListener("mousemove", stub());
-
-	const found = outsideEvents._events.find((event) => event.type === "mousemove");
-
-	expect(found).toBeDefined();
-});
-
-test("removeEventListener removes listener from document", () => {
-	spy(document, "removeEventListener");
-
-	outsideEvents.removeEventListener("click");
-	expect(document.removeEventListener.callCount).toBe(1);
-	expect(document.removeEventListener.args[0][0]).toEqual("click");
-	expect(typeof document.removeEventListener.args[0][1]).toEqual("function");
-
-	document.removeEventListener.restore();
-});
-
-test("removeEventListener removes listener meta data from _events array", () => {
-	outsideEvents.removeEventListener("mousemove");
-
-	const found = outsideEvents._events.find((event) => event.type === "mousemove");
-
-	expect(found).toBeUndefined();
-});
-
-test("nested elements don't trigger listeners", () => {
-	const el = document.querySelector(".nested");
-	el.dispatchEvent(event);
-
-	expect(listener.called).toBe(false);
-});
-
-test("events on self don't trigger listeners", () => {
-	const el = document.querySelector(".main");
-	el.dispatchEvent(event);
-
-	expect(listener.called).toBe(false);
-});
-
-test("events on ancestors trigger listeners", () => {
-	let el = document.querySelector(".outer");
-	el.dispatchEvent(event);
-
-	expect(listener.callCount).toBe(1);
-
-	el = document.querySelector("body");
-	el.dispatchEvent(event);
-
-	expect(listener.callCount).toBe(2);
+		expect(document.querySelector(".nested").innerHTML).toBe("0");
+	});
+	
+	test("events on ancestors trigger listeners", () => {
+		let el = document.querySelector(".outer");
+		el.dispatchEvent(event);
+	
+		expect(document.querySelector(".nested").innerHTML).toBe("1");
+	
+		el = document.querySelector("body");
+		el.dispatchEvent(event);
+	
+		expect(document.querySelector(".nested").innerHTML).toBe("2");
+	});
 });
